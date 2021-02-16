edd189d0dcb1090c010b4a1afe33b68c15d15cf6
Bobby Stefanov <boretobg@gmail.com>
2021-02-16 12:30:19 +0200

    01.InitialSetup Done

---
+++ 01.ADO-NET/01.InitialSetup/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+namespace _01.InitialSetup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connection = "Server=.\\SQLEXPRESS;Integrated Security=true;Database=MinionsDB";
+
+            using (var sqlConnection = new SqlConnection(connection))
+            {
+                sqlConnection.Open();
+            }
+        }
+    }
+}
