cc7d2ac13d2248532507e9e8553945b3baab3dd9
Bobby <boretobg@gmail.com>
2021-02-27 15:49:25 +0200

    done, not judge verified

---
+++ 04.EntityRelations/P01_StudentSystem/Data/Models/Homework.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace P01_StudentSystem.Data.Models
+{
+    public class Homework
+    {
+        public int HomeworkId { get; set; }
+
+        [Column(TypeName = "varchar(255)"), Required]
+        public string Content { get; set; }
+
+        public ContentType ContentType { get; set; }
+
+        public DateTime SubmissionTime { get; set; }
+
+        public int StudentId { get; set; }
+        public Student Student { get; set; }
+
+        public int CourseId { get; set; }
+        public Course Course { get; set; }
+    }
+}
+//o HomeworkId
+//o	Content (string, linking to a file, not unicode)
+//o ContentType(enum – can be Application, Pdf or Zip)
+//o SubmissionTime
+//o	StudentId
+//o	CourseId
+
---
+++ 04.EntityRelations/P01_StudentSystem/Data/Models/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace P01_StudentSystem.Data.Models
+{
+    public class Student
+    {
+
+        public Student()
+        {
+            this.CourseEnrollments = new HashSet<StudentCourse>();
+            this.HomeworkSubmissions = new HashSet<Homework>();
+        }
+
+        public int StudentId { get; set; }
+
+        [MaxLength(100), Required]
+        public string Name { get; set; }
+
+        [Column(TypeName = "char(10)")]
+        public int PhoneNumber { get; set; }
+
+        [Required]
+        public DateTime RegisteredOn { get; set; }
+        public DateTime? Birthday { get; set; }
+
+        public ICollection<StudentCourse> CourseEnrollments { get; set; }
+        public ICollection<Homework> HomeworkSubmissions { get; set; }
+    }
+}
