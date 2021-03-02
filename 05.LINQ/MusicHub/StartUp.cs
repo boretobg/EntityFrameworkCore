daef8db00aa549e89449761186194486a0024909
Bobby <boretobg@gmail.com>
2021-03-02 21:21:50 +0200

    All tasks done

--- 05.LINQ/MusicHub/StartUp.cs
+++ 05.LINQ/MusicHub/StartUp.cs
@@ -5,6 +5,7 @@
     using System.Text;
     using Data;
     using Initializer;
+    using Microsoft.EntityFrameworkCore;
 
     public class StartUp
     {
@@ -69,7 +70,41 @@
 
         public static string ExportSongsAboveDuration(MusicHubDbContext context, int duration)
         {
-            throw new NotImplementedException();
+            var songs = context.Songs
+                .Include(x => x.Album)
+                .ThenInclude(x => x.Producer)
+                .Include(x => x.Writer)
+                .ToList()   
+                .Where(x => x.Duration.TotalSeconds > duration)
+                .Select(x => new
+                {
+                    SongName = x.Name,
+                    PerformerFullName = x.SongPerformers.Select(x => $"{x.Performer.FirstName} {x.Performer.LastName}")
+                    .FirstOrDefault(),
+                    WriterName = x.Writer.Name,
+                    AlbumProducer = x.Album.Producer.Name,
+                    x.Duration
+                })
+                .OrderBy(x => x.SongName)
+                .ThenBy(x => x.WriterName)
+                .ThenBy(x => x.PerformerFullName)
+                .ToList();
+
+            StringBuilder sb = new StringBuilder();
+
+            int count = 1;
+
+            foreach (var song in songs)
+            {
+                sb.AppendLine($"-Song #{count++}")
+                  .AppendLine($"---SongName: {song.SongName}")
+                  .AppendLine($"---Writer: {song.WriterName}")
+                  .AppendLine($"---Performer: {song.PerformerFullName}")
+                  .AppendLine($"---AlbumProducer: {song.AlbumProducer}")
+                  .AppendLine($"---Duration: {song.Duration:c}");
+            }
+
+            return sb.ToString().TrimEnd();
         }
     }
 }
