0dd70a27b967e38458855f35a8467f6b3d69f8a3
Bobby <boretobg@gmail.com>
2021-03-05 16:34:27 +0200

    01.AgeRestriction 100/100

---
+++ 06.AdvancedQuerying/BookShopSystem/BookShop/StartUp.cs
@@ -0,0 +1,34 @@
+﻿namespace BookShop
+{
+    using Data;
+    using Initializer;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+        }
+
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            var bookTitles = context.Books
+                .OrderBy(x => x.Title)
+                .Where(x => x.AgeRestriction.ToString().ToLower() == command.ToLower())
+                .Select(x => new { Title = x.Title})
+                .ToList();
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var tittle in bookTitles)
+            {
+                sb.AppendLine(tittle.Title);
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
