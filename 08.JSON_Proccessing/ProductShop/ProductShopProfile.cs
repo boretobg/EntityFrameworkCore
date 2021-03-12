9b83dc12a68ed288bdb08451f69ce1e4127401ea
Bobby <boretobg@gmail.com>
2021-03-12 23:09:33 +0200

    01.ImportUsers Done

---
+++ 08.JSON_Proccessing/ProductShop/ProductShopProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using ProductShop.Models;
+using ProductShop.Models.DataTransferObjects;
+
+namespace ProductShop
+{
+    public class ProductShopProfile : Profile
+    {
+        public ProductShopProfile()
+        {
+            this.CreateMap<UserInputModel, User>();
+        }
+    }
+}
---
+++ 08.JSON_Proccessing/ProductShop/StartUp.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using AutoMapper;
+using Newtonsoft.Json;
+using ProductShop.Data;
+using ProductShop.Models;
+using ProductShop.Models.DataTransferObjects;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        static IMapper mapper;
+
+        public static void Main(string[] args)
+        {
+            var context = new ProductShopContext();
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            string inputJson = File.ReadAllText("../../../Datasets/users.json");
+
+            string result = ImportUsers(context, inputJson);
+
+            Console.WriteLine(result);
+        }
+
+        public static string ImportUsers(ProductShopContext context, string inputJson)
+        {
+            InitializeAutoMapper();
+
+            var dtoUsers = JsonConvert.DeserializeObject<IEnumerable<UserInputModel>>(inputJson);
+
+            var users = mapper.Map<IEnumerable<User>>(dtoUsers);
+
+            context.Users.AddRange(users);
+            context.SaveChanges();
+
+            return $"Successfully imported {users.Count()}";
+        }
+        private static void InitializeAutoMapper()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<ProductShopProfile>();
+            });
+
+            mapper = config.CreateMapper();
+        }
+    }
+}
