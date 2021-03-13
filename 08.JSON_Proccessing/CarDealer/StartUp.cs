db9041ebc752fd5cec1c8e77ebaa67ad22477a77
Bobby <boretobg@gmail.com>
2021-03-13 17:14:06 +0200

    10.ImportParts Done

--- 08.JSON_Proccessing/CarDealer/StartUp.cs
+++ 08.JSON_Proccessing/CarDealer/StartUp.cs
@@ -13,6 +13,25 @@ namespace CarDealer
     {
         public static void Main(string[] args)
         {
+            var context = new CarDealerContext();
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            string json = File.ReadAllText("../../../Datasets/parts.json");
+           string asd = ImportParts(context, json);
+            Console.WriteLine(asd);
+        }
+
+        public static string ImportParts(CarDealerContext context, string inputJson)
+        {
+            var suppliers = context.Suppliers.Select(x => x.Id).ToList();
+
+            var parts = JsonConvert.DeserializeObject<Part[]>(inputJson).Where(x => suppliers.Contains(x.SupplierId)).ToList();
+
+            context.Parts.AddRange(parts);
+            context.SaveChanges();
+
+            return $"Successfully imported {parts.Count()}.";
         }
 
         public static string ImportSuppliers(CarDealerContext context, string inputJson)
