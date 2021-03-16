d307f7cb376caf9f5dd36558f5a75fd806520f9d
Bobby <boretobg@gmail.com>
2021-03-16 12:35:15 +0200

    01.ImportUsers Done

--- 09.XML_Proccessing/ProductShop/StartUp.cs
+++ 09.XML_Proccessing/ProductShop/StartUp.cs
@@ -1,10 +1,28 @@
-﻿namespace ProductShop
+﻿using ProductShop.Data;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace ProductShop
 {
     public class StartUp
     {
         public static void Main(string[] args)
         {
+            var context = new ProductShopContext();
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
 
+            var text = File.ReadAllText("../../../Datasets/users.xml");
+            var xml = XDocument.Parse(text);
+            System.Console.WriteLine(ImportUsers(context, text));
+        }
+
+        public static string ImportUsers(ProductShopContext context, string inputXml)
+        {
+            var xmlDoc = XDocument.Parse(inputXml);
+
+            return $"Successfully imported {xmlDoc.Root.Elements().Count()}";
         }
     }
 }
