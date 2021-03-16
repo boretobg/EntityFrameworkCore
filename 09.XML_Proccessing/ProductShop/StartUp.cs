a0374e8417ed28312ff3bf3048570dc3c86a9667
Bobby <boretobg@gmail.com>
2021-03-16 22:25:58 +0200

    02.ImportProducts Done

--- 09.XML_Proccessing/ProductShop/StartUp.cs
+++ 09.XML_Proccessing/ProductShop/StartUp.cs
@@ -1,7 +1,10 @@
 ﻿using ProductShop.Data;
+using ProductShop.Dtos.Import;
+using ProductShop.Models;
 using System.IO;
 using System.Linq;
 using System.Xml.Linq;
+using System.Xml.Serialization;
 
 namespace ProductShop
 {
@@ -10,19 +13,42 @@ namespace ProductShop
         public static void Main(string[] args)
         {
             var context = new ProductShopContext();
-            context.Database.EnsureDeleted();
-            context.Database.EnsureCreated();
+            //context.Database.EnsureDeleted();
+           // context.Database.EnsureCreated();
 
-            var text = File.ReadAllText("../../../Datasets/users.xml");
+            var text = File.ReadAllText("../../../Datasets/products.xml");
             var xml = XDocument.Parse(text);
-            System.Console.WriteLine(ImportUsers(context, text));
+            System.Console.WriteLine(ImportProducts(context, text));
+        }
+
+        public static string ImportProducts(ProductShopContext context, string inputXml)
+        {
+            var xmlSerialized = new XmlSerializer(typeof(ProductInputModel[]), new XmlRootAttribute("Products"));
+
+            var textRead = new StringReader(inputXml);
+
+            var productsDto = xmlSerialized.Deserialize(textRead) as ProductInputModel[];
+
+            var products = productsDto.Select(x => new Product
+            {
+                Name = x.Name,
+                Price = x.Price,
+                SellerId = x.SellerId,
+                BuyerId = x.BuyerId
+            })
+                .ToList();
+
+            context.Products.AddRange(products);
+            context.SaveChanges();
+
+            return $"Successfully imported {products.Count}";
         }
 
         public static string ImportUsers(ProductShopContext context, string inputXml)
         {
-            var xmlDoc = XDocument.Parse(inputXml);
+            var users = XDocument.Parse(inputXml);
 
-            return $"Successfully imported {xmlDoc.Root.Elements().Count()}";
+            return $"Successfully imported {users.Root.Elements().Count()}";
         }
     }
 }
