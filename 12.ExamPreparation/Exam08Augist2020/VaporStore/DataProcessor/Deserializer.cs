5443c247d33c528755e60a229d45929090966e9a
Bobby <boretobg@gmail.com>
2021-03-29 21:50:28 +0300

    add first import method

--- 12.ExamPreparation/Exam08Augist2020/VaporStore/DataProcessor/Deserializer.cs
+++ 12.ExamPreparation/Exam08Augist2020/VaporStore/DataProcessor/Deserializer.cs
@@ -1,33 +1,78 @@
 ﻿namespace VaporStore.DataProcessor
 {
-	using System;
+    using System;
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Text;
     using Data;
+    using Newtonsoft.Json;
+    using VaporStore.Data.Models;
+    using VaporStore.DataProcessor.Dto;
 
-	public static class Deserializer
-	{
-		public static string ImportGames(VaporStoreDbContext context, string jsonString)
-		{
-			throw new NotImplementedException();
-		}
+    public static class Deserializer
+    {
+        public static string ImportGames(VaporStoreDbContext context, string jsonString)
+        {
+            var sb = new StringBuilder();
 
-		public static string ImportUsers(VaporStoreDbContext context, string jsonString)
-		{
-			throw new NotImplementedException();
-		}
+            var games = JsonConvert.DeserializeObject<IEnumerable<GameImportModel>>(jsonString);
 
-		public static string ImportPurchases(VaporStoreDbContext context, string xmlString)
-		{
-			throw new NotImplementedException();
-		}
+            foreach (var jsonGame in games)
+            {
+                if (!IsValid(jsonGame) || jsonGame.Tags.Count() == 0)
+                {
+                    sb.AppendLine("Invalid Data");
+                    continue;
+                }
 
-		private static bool IsValid(object dto)
-		{
-			var validationContext = new ValidationContext(dto);
-			var validationResult = new List<ValidationResult>();
+                var genre = context.Genres.FirstOrDefault(x => x.Name == jsonGame.Genre)
+                    ?? new Genre { Name = jsonGame.Genre };
 
-			return Validator.TryValidateObject(dto, validationContext, validationResult, true);
-		}
-	}
+                var developer = context.Developers.FirstOrDefault(x => x.Name == jsonGame.Developer)
+                    ?? new Developer { Name = jsonGame.Developer };
+
+                var game = new Game
+                {
+                    Name = jsonGame.Name,
+                    Price = jsonGame.Price,
+                    ReleaseDate = jsonGame.ReleaseDate.Value,
+                    Developer = developer,
+                    Genre = genre
+                };
+
+                foreach (var jsonTag in jsonGame.Tags)
+                {
+                    var tag = context.Tags.FirstOrDefault(x => x.Name == jsonTag)
+                        ?? new Tag { Name = jsonTag };
+                    game.GameTags.Add(new GameTag { Tag = tag });
+                }
+
+                context.Games.Add(game);
+                context.SaveChanges();
+
+                sb.AppendLine($"Added {jsonGame.Name} ({jsonGame.Genre}) with {jsonGame.Tags.Count()} tags");
+            }
+
+            return sb.ToString().TrimEnd();
+        }  
+
+        public static string ImportUsers(VaporStoreDbContext context, string jsonString)
+        {
+            return "";
+        }
+
+        public static string ImportPurchases(VaporStoreDbContext context, string xmlString)
+        {
+            return "";
+        }
+
+        private static bool IsValid(object dto)
+        {
+            var validationContext = new ValidationContext(dto);
+            var validationResult = new List<ValidationResult>();
+
+            return Validator.TryValidateObject(dto, validationContext, validationResult, true);
+        }
+    }
 }
---
+++ 12.ExamPreparation/Exam08Augist2020/VaporStore/DataProcessor/Dto/Import/GameImportModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using VaporStore.Data.Models;
+
+namespace VaporStore.DataProcessor.Dto
+{
+    public class GameImportModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Range(0, double.MaxValue)]
+        public decimal Price { get; set; }
+
+        [Required]
+        public DateTime? ReleaseDate { get; set; }
+
+        [Required]
+        public string Developer { get; set; }
+
+        [Required]
+        public string Genre { get; set; }
+
+        public IEnumerable<string> Tags { get; set; }
+    }
+}
