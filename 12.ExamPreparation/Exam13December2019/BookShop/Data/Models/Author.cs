8a1a6de4b5e16a6aa78a0ae367f2fd265acaaf5c
Bobby <boretobg@gmail.com>
2021-03-31 13:14:24 +0300

    Add Model Definition

---
+++ 12.ExamPreparation/Exam13December2019/BookShop/Data/Models/Author.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace BookShop.Data.Models
+{
+    public class Author
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+
+        public ICollection<AuthorBook> AuthorsBooks { get; set; }
+    }
+}
---
+++ 12.ExamPreparation/Exam13December2019/BookShop/Data/Models/Book.cs
@@ -0,0 +1,23 @@
+﻿using BookShop.Data.Models.Enums;
+using System;
+using System.Collections.Generic;
+
+namespace BookShop.Data.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public Genre Genre { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int Pages { get; set; }
+
+        public DateTime PublishedOn { get; set; }
+
+        public ICollection<AuthorBook> AuthorsBooks { get; set; }
+    }
+}
